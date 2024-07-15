f4626ba5cbe303cef4ebd851f721fbb90480e49e
priyal.finestar <priyal@finestar.in>
2024-07-15 10:26:45 +0530

    slider create using react slick

--- src/Components/SliderHero/SliderHero.jsx
+++ src/Components/SliderHero/SliderHero.jsx
@@ -9,50 +9,44 @@ import hero4 from "../../assets/SVG/hero-4.jpg";
 import hero5 from "../../assets/SVG/hero-5.jpg";
 import hero6 from "../../assets/SVG/hero-6.jpg";
 import hero7 from "../../assets/SVG/hero-7.jpg";
+import "./sliderhero.css"
 
 
 const SliderHero = () => {
-    const Arrow = ({ className, style, onClick, direction }) => (
-        <div
-            className={className}
-            style={{ ...style, display: 'block', background: direction === 'next' ? 'red' : 'green' }}
-            onClick={onClick}
-        />
-    );
     var settings = {
         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        nextArrow: <Arrow direction="next" />,
-        prevArrow: <Arrow direction="prev" />
     };
     return (
-        <Slider {...settings}>
-            {/* Add your slide content here */}
-            <div>
-                <img src={hero1} alt="Slide 1" />
-            </div>
-            <div>
-                <img src={hero2} alt="Slide 2" />
-            </div>
-            <div>
-                <img src={hero3} alt="Slide 3" />
-            </div>
-            <div>
-                <img src={hero4} alt="Slide 4" />
-            </div>
-            <div>
-                <img src={hero5} alt="Slide 5" />
-            </div>
-            <div>
-                <img src={hero6} alt="Slide 6" />
-            </div>
-            <div>
-                <img src={hero7} alt="Slide 7" />
-            </div>
-        </Slider>
+        <div className='slider-container'>
+            <Slider {...settings}>
+                <div className='carousel-slide'>
+                    <img src={hero1} alt="Slide 1" className="carousel-image" />
+                </div>
+                <div className='carousel-slide'>
+                    <img src={hero2} alt="Slide 2" className="carousel-image" />
+                </div>
+                <div className='carousel-slide'>
+                    <img src={hero3} alt="Slide 3" className="carousel-image" />
+                </div>
+                <div className='carousel-slide'>
+                    <img src={hero4} alt="Slide 4" className="carousel-image" />
+                </div>
+                <div className='carousel-slide'>
+                    <img src={hero5} alt="Slide 5" className="carousel-image" />
+                </div>
+                <div className='carousel-slide'>
+                    <img src={hero6} alt="Slide 6" className="carousel-image" />
+                </div >
+                <div className='carousel-slide'>
+                    <img src={hero7} alt="Slide 7" className="carousel-image"/>
+                </div>
+            </Slider>
+        </div>
+
     )
 }
 
