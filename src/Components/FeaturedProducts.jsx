67fd01a5e55924efdc1c45714ba04f7b296a9832
priyalgoti07 <gotipriyal1996@gmail.com>
2024-07-24 13:37:46 +0530

    featuredproducti slider create

--- src/Components/FeaturedProducts.jsx
+++ src/Components/FeaturedProducts.jsx
@@ -1,41 +1,41 @@
 import React, { useState } from 'react'
 import { allProductData } from '../data/productData'
 import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import ProgressiveImage from 'react-progressive-graceful-image';
 
 const FeaturedProducts = () => {
   const [featured] = useState([
     allProductData[11],
-    // allProductData[50],
-    // allProductData[75],
-    // allProductData[54],
-    // allProductData[23],
-    // allProductData[40],
+    allProductData[50],
+    allProductData[75],
+    allProductData[54],
+    allProductData[23],
+    allProductData[40],
   ])
 
   const productSettings = {
-    dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3
   }
   return (
-    <section className='container m-auto'>
+    <section className='container m-auto border border-solid'>
       <h3 className='tracking-widest font-bold text-2xl text-center'>NEW ARRIVALS</h3>
       <Slider {...productSettings}>
-        <div style={{ backgroundColor: "red", height: "200px" }}>
-          {featured.map((item) => {
-            console.log("item", item);
-            <img/>
-            // <div style={{ backgroundColor: "red" }}>
-            //   <img src={item.images.main} style={{ maxWidth: "200px" }} />
-            //   <h3 className="hero-item__title">{item.title}</h3>
-            //   <p className="hero-item__details">{item.details}</p>
-            // </div>
-          }
-          )}
-        </div>
+        {featured.map((item, index) => (
+          <div key={index} style={{ backgroundColor: "pink" }} className='border border-solid'>
+            <div>
+              <ProgressiveImage src={item.images.main} placeholder={item.compressedImages.main}>
+                {(src, loading) => <img src={src} alt={item.title} />}
+              </ProgressiveImage>
+              <h3>{item.title}</h3>
+              <p>{item.price}</p>
+            </div>
+          </div>
+        ))}
       </Slider>
     </section>
   )
