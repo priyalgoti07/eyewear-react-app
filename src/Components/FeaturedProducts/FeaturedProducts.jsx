eb6c0ad51328b8dbfb1947ca66b69a3a188f063f
priyalgoti07 <gotipriyal1996@gmail.com>
2024-07-25 21:47:43 +0530

    react slick slider completed

--- src/Components/FeaturedProducts/FeaturedProducts.jsx
+++ src/Components/FeaturedProducts/FeaturedProducts.jsx
@@ -5,8 +5,29 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ProgressiveImage from 'react-progressive-graceful-image';
 import "./featuredProducts.css"
+import left from "../../assets/SVG/arrows/left-arrow-svgrepo-com.svg"
+import right from "../../assets/SVG/arrows/right-arrow-svgrepo-com.svg"
+
+
 
 const FeaturedProducts = () => {
+
+  const NextArrow = (props) => {
+    const { onClick } = props;
+    return (
+      <div className="custom-arrow next-arrow" onClick={onClick}>
+        <img src={right} />
+      </div>
+    );
+  };
+  const PrevArrow = (props) => {
+    const { onClick } = props;
+    return (
+      <div className="custom-arrow prev-arrow" onClick={onClick}>
+        <img src={left} />
+      </div>
+    );
+  };
   const [featured] = useState([
     allProductData[11],
     allProductData[50],
@@ -22,6 +43,8 @@ const FeaturedProducts = () => {
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
   }
   return (
     <section className='container m-auto'>
@@ -29,7 +52,7 @@ const FeaturedProducts = () => {
       <Slider {...productSettings}>
         {featured.map((item, index) => (
           <div key={index} >
-            <div className='m-10 text-center '>
+            <div className='m-5 text-center '>
               <ProgressiveImage src={item.images.main} placeholder={item.compressedImages.main}>
                 {(src, loading) =>
                   <div className='featured-pro-img'>
@@ -37,7 +60,7 @@ const FeaturedProducts = () => {
                   </div>}
               </ProgressiveImage>
               <div className='flex flex-col gap-2 hover:bg-[#f2efee] p-2'>
-                <h3>{item.title}</h3>
+                <h3 className='text-lg font-semibold tracking-widest uppercase text-[#423c3a]'>{item.title}</h3>
                 <p>{item.price}</p>
               </div>
             </div>
