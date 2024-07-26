2093690e0c386c6c9959c68ee8c0e8fc15d22940
priyalgoti07 <gotipriyal1996@gmail.com>
2024-07-26 22:11:32 +0530

    products img simple css add

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { allProductData } from '../../data/productData'
 
 const Product = () => {
@@ -17,21 +17,27 @@ const Product = () => {
     const displayItem = productData.find((item) => item.id === id)
     console.log("displayItem", displayItem);
     const [toggleImg, setToggleImg] = useState(displayItem.images?.main)
+
+    console.log("setToggleImg", setToggleImg);
     return (
         <div className='container m-auto '>
             <div className='flex px-10 py-12'>
                 <div className='flex flex-col gap-2'>
                     <ul className='flex gap-2 text-[#777777] uppercase text-[13px] font-semibold'>
-                        <li className='underline underline-offset-[3px]'>Home</li><span>/</span>
-                        <li className='underline underline-offset-[3px]'>Sunglasses</li><span>/</span>
+                        <Link to="/">
+                            <li className='underline underline-offset-[3px] inline-block'>Home</li> {" /"}
+                        </Link>
+                        <Link>
+                            <li className='underline underline-offset-[3px] inline-block'>Sunglasses</li> {" /"}
+                        </Link>
                         <li>{displayItem?.brand}</li>
                     </ul>
-                    <div>
-                        <img src={toggleImg} className='max-w-[550px] border-2' />
+                    <div className='max-w-[786px] transition-transform duration-300 ease-linear'>
+                        <img src={toggleImg} className='border-2 w-[786px] max-h-[500px] object-cover hover: transform scale-105 cursor-pointer' />
                     </div>
                     <div className='flex justify-center gap-5'>
-                        <img src={displayItem.images?.main} className='max-w-[90px] border-2' onClick={() => setToggleImg(displayItem.images?.main)} />
-                        <img src={displayItem.images?.side} className='max-w-[90px] border-2' onClick={() => setToggleImg(displayItem.images?.side)} />
+                        <img src={displayItem.images?.main} className={`max-w-[90px] border-2 ${toggleImg === displayItem.images?.main ? 'border-[#C8651B]' : ''}`} onClick={() => setToggleImg(displayItem.images?.main)} />
+                        <img src={displayItem.images?.side} className={`max-w-[90px] border-2 ${toggleImg === displayItem.images?.side ? 'border-[#C8651B]' : ''}`} onClick={() => setToggleImg(displayItem.images?.side)} />
                     </div>
                 </div>
                 <div>Content</div>
