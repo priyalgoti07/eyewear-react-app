a87b7f31e6a9cb402bc1a7088ca560964ea35882
priyal.finestar <priyal@finestar.in>
2024-10-16 13:21:08 +0530

    To change the title of a webpage dynamically only when the URL changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Components/Header/Header'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Footer from './Components/Footer/Footer'
 
 const Layout = () => {
+    console.log("Layout")
+    const location = useLocation();
+
+    useEffect(() => {
+        // Set document title based on the current route
+        switch (location.pathname) {
+            case '/':
+                document.title = 'Home - My App';
+                break;
+            case '/cart':
+                document.title = 'Cart - My App';
+                break;
+            case '/products':
+                document.title = 'Products - My App';
+                break;
+            case '/products/eyeglasses':
+                document.title = 'Eyeglasses - My App';
+                break;
+            case '/products/sunglasses':
+                document.title = 'Sunglasses - My App';
+                break;
+            default:
+                if (location.pathname.startsWith('/products/')) {
+                    document.title = 'Product Details - My App';
+                } else {
+                    document.title = 'My App';
+                }
+        }
+    }, [location]);
     return (
         <>
             <Header />
