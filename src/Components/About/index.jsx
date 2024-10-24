9a8d686904fb717c254fc5caac06b90d7ec1608d
priyalgoti07 <gotipriyal1996@gmail.com>
2024-10-24 08:22:43 +0530

    about first setion is completed

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <>
-      <div>I am About Page</div>
-      <div>I am About Page</div>
-    </>
-
+    <div className=' m-auto text-[#423C3A] py-8 '>
+      <section className='text-center flex py-8 flex-col gap-8 justify-center items-center'>
+        <h1 className='tracking-[3px] uppercase text-[22px] font-black mt-10'>An Eye for Better Eyewear</h1>
+        <p className='max-w-[600px] text-sm font-Raleway tracking-[.5px] leading-6 font-medium'>Our designs are carefully crafted to capture the latest fashion trends while ensuring maximum comfort and functionality.
+          With a range of unique styles and color options, we are confident that our collection has something to suit every taste and personality.</p>
+        <p className='max-w-[600px] text-sm font-Raleway tracking-[.5px] leading-6 font-medium'>Our frames are crafted with attention to detail, utilizing the latest materials and technology to ensure the highest quality products.
+          Elevate your eyewear game with SAVANT frames.</p>
+      </section>
+    </div>
   )
 }
 
